0b9dc54037c2a3d1ac26d106638cef9eaed8adac
MPlugaru89 <mihaiplugaru89@gmail.com>
2023-05-03 18:18:05 +0200

    adaugare body

--- app.js
+++ app.js
@@ -3,11 +3,49 @@ console.log("Hello!");
 yargs.version('1.1.0');
 
 yargs.command({
-    command: "add",
-    describe: "Adaugarea unei notite",
-    handler: function() {
-        console.log('Adaugare notita');
-    }
+	command: 'add',
+	describe: 'Adaugare unei notite',
+	builder: {
+		title: {
+			describe: 'Titlul notitei',
+			demandOption: true,
+			type: 'string'
+		},
+        body: {
+            describe: 'Continutul notitei',
+            demandOption: true,
+            type: 'string'
+        }
+	},
+	handler: function (argv) {
+		console.log(`Adauga notite cu titlul: ${argv.title} `, `cu continutul ${argv.body} `);
+	}
 })
 
-console.log(yargs.argv);
+yargs.command({
+	command: 'remove',
+	describe: 'Sterge o notita',
+	handler: function() {
+		console.log('sterge notita');
+	}
+})
+
+yargs.command({
+	command: 'list',
+	describe: 'prezinta lista de notite',
+	handler: function() {
+		console.log('lista de notite');
+	}
+})
+
+yargs.command({
+	command: 'read',
+	describe: 'citeste o notita',
+	handler: function() {
+		console.log('prezinta notita');
+	}
+})
+
+yargs.parse();
+
+
