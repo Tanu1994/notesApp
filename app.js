05e01fb94dba5a6bc7d7f1eaad8e4641c6e4a395
Vlad Cervac <cervacvad32@gmail.com>
2023-05-03 17:50:46 +0200

    added 4 comand and title flag

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ console.log("Hello!");
 yargs.version('1.1.0');
 
 yargs.command({
-    command: "add",
-    describe: "Adaugarea unei notite",
-    handler: function() {
-        console.log('Adaugare notita');
-    }
+	command: 'add',
+	describe: 'Adaugare unei notite',
+	builder: {
+		title: {
+			describe: 'Titlul notitei',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		console.log(`Adauga notite cu titlul: ${argv.title} `);
+	}
+})
+yargs.command({
+	command: 'remove',
+	describe: 'Sterge o notita',
+	handler: function() {
+		console.log('sterge notita');
+	}
+})
+yargs.command({
+	command: 'list',
+	describe: 'prezinta lista de notite',
+	handler: function() {
+		console.log('lista de notite');
+	}
+})
+yargs.command({
+	command: 'read',
+	describe: 'citeste o notita',
+	handler: function() {
+		console.log('prezinta notita');
+	}
 })
 
-console.log(yargs.argv);
+yargs.parse();
