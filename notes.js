99d27a041b6f75c554e47d7a058656f98d459932
Elena <ririteaca@gmail.com>
2023-05-04 18:56:54 +0300

    functionalitate Load Notes adaugata

--- notes.js
+++ notes.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataString = dataBuffer.toString();
+        return JSON.parse(dataString);
+    } catch(e){
+        return [];
+    }
+    
+     
+}
+
 const addNote = function(title, body) {
     console.log(`Adauga notite cu titlul: ${title} `, `cu continutul ${body} `);
+
+    // citim datele din fisier
+    const notes = loadNotes();
+    console.log(notes);
+    // verificam existenta notitei in fisier
+    // adaugam notita la obiectul javascript, si modificam fisierul
+
     
 }
 
